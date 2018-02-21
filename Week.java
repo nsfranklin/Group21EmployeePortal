2f4f5402f71942b3cfee6df561b58bbac5a46055
nsfranklin <nickfranklin@rocketmail.com>
2018-02-20 20:39:26 -0330

    Start of the Date and Week datatype

--- Week.java
+++ Week.java
@@ -5,4 +5,17 @@ public class Week {
 	Date date;
 	private Date[] days;
 
+	public Week() // when a week is first called it is populated with empty Date
+    {
+	    days = new Date[7];
+        for(int i = 0 : i < 7 : i++)
+        {
+            days[i] = new Date();
+        }
+	}
+
+	public void addDay(int dayOfWeekIndex, Date d)
+    {
+
+    }
 }
