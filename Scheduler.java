4f1f2906c2fa263db573ad3efc7a84cfde2dbe4d
nsfranklin <nickfranklin@rocketmail.com>
2018-03-14 21:12:45 +0000

    More IO and Scheduler

--- Scheduler.java
+++ Scheduler.java
@@ -8,61 +8,45 @@ public class Scheduler {
 	private Week unapprovedSchedule;
 	private static Scheduler instance;
 
-	public Week createProvisionalSchedule(Date[][] prov, Week scheduleRules, ArrayList<Employee> ActiveUsers) {
-		ArrayList<Employee> timetabledEmployees = ActiveUsers;
-	    Week tempWeek = null;
-	    for(int x = ActiveUsers.size() ; x > 0 ; x++)
-		{
-            tempWeek = ActiveUsers.get(x).getEmployeeAvalibility();
-            if((tempWeek = null) || (tempWeek.getWeek.size !=7))  //preventing errors and crashes
-            {
-
-            }
-		}
-	}
-
-	public Week getSetScheduleRules() {
-		// TODO - implement Scheduler.getSetScheduleRules
+	public Scheduler() {
+		// TODO - implement Scheduler.Scheduler
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param scheduleRules
-	 */
+	public Week createProvisionalSchedule(Date[][] prov, Week scheduleRules, ArrayList<Employee> activeUsers) {
+		ArrayList<Employee> timetabledEmployees = ActiveUsers;
+	    Week tempWeek = addFullTimeEmployees(activeUsers);
+
+	}
+
+	public Week addFullTimeEmployees(ArrayList<Employee> a) {
+		for(int i = 0 ; i < a.size() ; i++){
+			if(!(a.get(i) instanceof  PartTimeEmployee)){
+
+			}
+		}
+	}
+
+	public Week getScheduleRules() {
+		return this.getScheduleRules();
+	}
+
 	public void setScheduleRules(Week scheduleRules) {
 		this.scheduleRules = scheduleRules;
 	}
 
-	/**
-	 * 
-	 * @param managersAlteredSchedule
-	 */
 	public Boolean checkSchedule(Week managersAlteredSchedule) {
 		// TODO - implement Scheduler.checkSchedule
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param finalSchedule
-	 */
 	public Boolean approveSchedule(Week finalSchedule) {
 		// TODO - implement Scheduler.approveSchedule
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param deadline
-	 */
 	public void createSystemRequest(int deadline) {
 		// TODO - implement Scheduler.createSystemRequest
-		throw new UnsupportedOperationException();
-	}
-
-	public Scheduler() {
-		// TODO - implement Scheduler.Scheduler
 		throw new UnsupportedOperationException();
 	}
 
